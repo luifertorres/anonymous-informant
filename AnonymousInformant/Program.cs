8b2dd7aa2dc77654c8e693992b4a90dbbd0ec9a7
Luis Torres <luis.torres.dev@outlook.com>
2024-03-31 20:08:17 -0500

    Solve the problem

---
+++ AnonymousInformant/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace AnonymousInformant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var testCases = int.Parse(Console.ReadLine());
+
+            while (testCases-- > 0)
+            {
+                var nk = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                var n = nk[0];
+                var k = nk[1];
+                var b = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                var canAnonymousInformantWordsBeTrue = true;
+                var lastIndex = n - 1;
+
+                k = Math.Min(k, n);
+
+                for (var i = 0; i < k; i++)
+                {
+                    var rightShiftsToDo = b[lastIndex];
+
+                    if (rightShiftsToDo > n)
+                    {
+                        canAnonymousInformantWordsBeTrue = false;
+                        break;
+                    }
+
+                    lastIndex = (lastIndex + n - rightShiftsToDo) % n;
+                }
+
+                Console.WriteLine(canAnonymousInformantWordsBeTrue ? "Yes" : "No");
+            }
+        }
+    }
+}
